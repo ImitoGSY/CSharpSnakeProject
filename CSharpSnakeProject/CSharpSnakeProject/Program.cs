97ccf047995e45c392ac189a42e0b2d1130ab255
Станис <102852335+ImitoGSY@users.noreply.github.com>
2024-09-17 23:18:18 +1000

    1'st iteration

---
+++ CSharpSnakeProject/CSharpSnakeProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using mySnake.shared;
+using mySnake.snake;
+using Shared;
+using System.Diagnostics;
+
+internal class Program 
+{
+    static void Main()
+    {
+
+        var gameLogic = new SnakeGameLogic();
+
+        var input = new ConsoleInput();
+        gameLogic.InitializeInput(input);
+        gameLogic.GotoGameplay();
+
+        var lastFrameTime = DateTime.Now;
+        while (true)
+        {
+            input.Update();
+            var frameStartTime = DateTime.Now;
+            float deltaTime = (float)(frameStartTime - lastFrameTime).TotalSeconds;
+            gameLogic.Update(deltaTime);
+            lastFrameTime = frameStartTime;
+        }
+    }
+
+
+}
